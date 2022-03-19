596a390a2b0150e87f27074108c7b6a1d3beb5f4
Dmitry <dmitrycrown@yandex.ru>
2022-03-19 15:43:41 +0300

    Add handling post query

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const port = process.env.PORT || 8000
+const https = require('https')
 
 const app = express()
 
@@ -7,9 +8,31 @@ app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.use(express.static('public'))
 
+
 app.get('/', (req, res) => {
   res.sendFile(`${__dirname}/public/index.html`)
 })
+
+
+app.post('/', (req, res) => {
+  const message = `
+  Message from ${req.body.name}
+  Email: req.body.email
+  Feedback: ${req.body.feedback}`
+
+  send_telegram_message(message)
+
+  res.status(201).sendFile(`${__dirname}/public/index.html`)
+})
+
+
+function send_telegram_message(message) {
+  const token = process.env.TOKEN
+  const chat_id = process.env.CHAT_ID
+
+  const url_req = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=${message}`
+  https.get(url_req)
+}
 
 
 app.listen(port, () => {
